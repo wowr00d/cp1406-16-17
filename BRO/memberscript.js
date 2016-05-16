f19ae843783ad1f24f06cf68beee33242b75e16b
JustinNix <justin.nix@my.jcu.edu.au>
2016-05-16 13:16:04 +1000

    Forms
    I have tried uploading these lots, however this time i deleted the repo
    and re cloned and i have had to drag and drop files so if anyone edited
    my previous form files on their wont have those edits. but if you didnt
    then thats fine,

--- BRO/memberscript.js
+++ BRO/memberscript.js
@@ -1,15 +1,18 @@
+/*make a progress bar for completion*/
+/*disable option for things which are relevant*/
+
 
 function nameEntered(name) {
     if (name.value != "") {
-        document.BenefactorForm.nameTick.src = "images/tick.png";
-        document.BenefactorForm.nameTick.alt = "tick";
+        document.memberForm.nameTick.src = "images/tick.png";
+        document.memberForm.nameTick.alt = "tick";
         document.getElementById("nameErrMsg").innerHTML = "Perfect";
         namex = true;
         return true
     }
     else {
-        document.BenefactorForm.nameTick.src = "images/cross.png";
-        document.BenefactorForm.nameTick.alt = "cross";
+        document.memberForm.nameTick.src = "images/cross.png";
+        document.memberForm.nameTick.alt = "cross";
         document.getElementById("submit").disabled = true;
         namex = false;
         return false
@@ -35,19 +38,19 @@ function donationEntered(Donation) {
 } 
 
 function emailEntered(Email) {
-    var x = document.forms["BenefactorForm"]["Email"].value;
+    var x = document.forms["memberForm"]["Email"].value;
     var atpos = x.indexOf("@");
     var dotpos = x.lastIndexOf(".");
     if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {
-        document.BenefactorForm.emailTick.src = "images/cross.png";
-        document.BenefactorForm.emailTick.alt = "cross";
+        document.memberForm.emailTick.src = "images/cross.png";
+        document.memberForm.emailTick.alt = "cross";
         document.getElementById("emailErrMsg") = "Not a valid e-mail address"
         emailx = false;
         return false
     }
     else{
-        document.BenefactorForm.emailTick.src = "images/tick.png";
-        document.BenefactorForm.emailTick.alt = "tick";
+        document.memberForm.emailTick.src = "images/tick.png";
+        document.memberForm.emailTick.alt = "tick";
         document.getElementById("emailErrMsg") = "Perfect"
         emailx = true;
         return true
@@ -57,15 +60,15 @@ function emailEntered(Email) {
 function phoneEntered(Phone) {  
   var phoneno = /^\d{10}$/;
   if(Phone.value.match(phoneno)){  
-      document.BenefactorForm.phoneTick.src = "images/tick.png";
-      document.BenefactorForm.phoneTick.alt = "tick";
+      document.memberForm.phoneTick.src = "images/tick.png";
+      document.memberForm.phoneTick.alt = "tick";
       document.getElementById("phoneErrMsg").innerHTML = "Perfect";
       phonex= true;
       return true
   }  
   else{  
-      document.BenefactorForm.phoneTick.src = "images/cross.png";
-      document.BenefactorForm.phoneTick.alt = "cross";
+      document.memberForm.phoneTick.src = "images/cross.png";
+      document.memberForm.phoneTick.alt = "cross";
       document.getElementById("phoneErrMsg").innerHTML = "Please enter only 10 digits without spacing or other special characters";
      phonex= false;  
      return false
@@ -75,15 +78,15 @@ function phoneEntered(Phone) {
 function mobileEntered(Mobile) {  
   var mobileno = /^\d{10}$/;
   if(Mobile.value.match(phoneno)){  
-      document.BenefactorForm.mobileTick.src = "images/tick.png";
-      document.BenefactorForm.mobileTick.alt = "tick";
+      document.memberForm.mobileTick.src = "images/tick.png";
+      document.memberForm.mobileTick.alt = "tick";
       document.getElementById("mobileErrMsg").innerHTML = "Perfect";
       mobilex= true;
       return true
   }  
   else{  
-      document.BenefactorForm.mobileTick.src = "images/cross.png";
-      document.BenefactorForm.mobileTick.alt = "cross";
+      document.memberForm.mobileTick.src = "images/cross.png";
+      document.memberForm.mobileTick.alt = "cross";
       document.getElementById("mobileErrMsg").innerHTML = "Please enter only 10 digits without spacing or other special characters";
      mobilex= false;  
      return false
@@ -103,15 +106,15 @@ function addressEntered(Address){
 
 function addressEntered(Address){
   if(Address.value != ""){
-    document.BenefactorForm. addressTick.src = "images/tick.png";
-    document.BenefactorForm.addressTick.alt = "tick";
+    document.memberForm. addressTick.src = "images/tick.png";
+    document.memberForm.addressTick.alt = "tick";
     document.getElementById("addressErrMsg").innerHTML = "Perfect";
     addressx= true;
     return true
   }
   else{
-    document.BenefactorForm. addressCross.src = "images/cross.png";
-    document.BenefactorForm.addressCross.alt = "cross";
+    document.memberForm. addressCross.src = "images/cross.png";
+    document.memberForm.addressCross.alt = "cross";
     document.getElementById("addressErrMsg").innerHTML = "Please Enter a valid address";
     addressx= false;
     return false
@@ -138,12 +141,12 @@ function anonUnDisable(anonymous) {
 
 function nameEntered(name) {
     if (name.value != "") {
-        document.BenefactorForm.nameTick.src = "images/tick.png";
-        document.BenefactorForm.nameTick.alt = "tick";
+        document.memberForm.nameTick.src = "images/tick.png";
+        document.memberForm.nameTick.alt = "tick";
     }
     else {
-        document.BenefactorForm.nameTick.src = "images/cross.png";
-        document.BenefactorForm.nameTick.alt = "cross";
+        document.memberForm.nameTick.src = "images/cross.png";
+        document.memberForm.nameTick.alt = "cross";
     }
 }
 
@@ -160,7 +163,7 @@ function donationEntered(Donation) {
 } 
 
 function emailEntered(Email) {
-    var x = document.forms["BenefactorForm"]["Email"].value;
+    var x = document.forms["memberForm"]["Email"].value;
     var atpos = x.indexOf("@");
     var dotpos = x.lastIndexOf(".");
     if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {
@@ -172,14 +175,14 @@ function emailEntered(Email) {
 function phoneEntered(Phone) {  
   var phoneno = /^\d{10}$/;
   if(Phone.value.match(phoneno)){  
-      document.BenefactorForm.phoneTick.src = "images/tick.png";
-      document.BenefactorForm.phoneTick.alt = "tick";
+      document.memberForm.phoneTick.src = "images/tick.png";
+      document.memberForm.phoneTick.alt = "tick";
       document.getElementById("phoneErrMsg").innerHTML = "Perfect";
       return true;
   }  
   else{  
-      document.BenefactorForm.phoneTick.src = "images/cross.png";
-      document.BenefactorForm.phoneTick.alt = "cross";
+      document.memberForm.phoneTick.src = "images/cross.png";
+      document.memberForm.phoneTick.alt = "cross";
       document.getElementById("phoneErrMsg").innerHTML = "Please enter only 10 digits without spacing or other special characters";
      return false;  
   }  
@@ -198,13 +201,13 @@ function addressEntered(Address){
 
 function addressEntered(Address){
   if(Address.value != ""){
-    document.BenefactorForm. addressTick.src = "images/tick.png";
-    document.BenefactorForm.addressTick.alt = "tick";
+    document.memberForm. addressTick.src = "images/tick.png";
+    document.memberForm.addressTick.alt = "tick";
     document.getElementById("addressErrMsg").innerHTML = "Perfect";
   }
   else{
-    document.BenefactorForm. addressCross.src = "images/cross.png";
-    document.BenefactorForm.addressCross.alt = "cross";
+    document.memberForm. addressCross.src = "images/cross.png";
+    document.memberForm.addressCross.alt = "cross";
     document.getElementById("addressErrMsg").innerHTML = "Please Enter a valid address";
   }
 }
@@ -218,3 +221,26 @@ function anonUnDisable(anonymous) {
   document.getElementById("recognition").disabled = false;
 }
 
+function familyTicket(){
+  document.getElementById("type").disabled =true;
+}
+function notfamily(){
+  document.getElementById("kidname1").disabled =true;
+  document.getElementById("kidname2").disabled =true;
+  document.getElementById("kidname3").disabled =true;
+  document.getElementById("kidDate1").disabled=true;
+  document.getElementById("kidDate2").disabled=true;
+  document.getElementById("kidDate3").disabled=true;
+  document.getElementById("kidname1").innerHTML ="";
+  document.getElementById("kidname2").innerHTML ="";
+  document.getElementById("kidname3").innerHTML ="";
+  document.getElementById("kidDate1").innerHTML="";
+  document.getElementById("kidDate2").innerHTML="";
+  document.getElementById("kidDate3").innerHTML="";
+}
+function disablePlayReg(){
+  document.getElementById("instrument").disabled=true;
+  document.getElementById("qualification").disabled=true;
+  document.getElementById("experience").disabled=true;
+
+}
